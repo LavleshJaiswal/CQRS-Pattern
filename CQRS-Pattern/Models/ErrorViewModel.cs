934e9505585c3e482e5e99174845756d3e816b23
LavleshJaiswal <lavlesh08mey@gmail.com>
2025-01-01 21:39:39 +0530

    CQRS pattern commit

---
+++ CQRS-Pattern/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CQRS_Pattern.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
